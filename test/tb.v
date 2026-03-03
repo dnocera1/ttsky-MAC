1207ab0abc3d2b7179f7df4eb123033c032a18ab
dnocera <dnocera@ucsc.edu>
2026-03-02 21:45:21 -0800

    docs and test actions fix

--- test/tb.v
+++ test/tb.v
@@ -28,7 +28,7 @@ module tb ();
 `endif
 
   // Replace tt_um_example with your module name:
-  MAC user_project (
+  tt_um_MAC user_project (
 
       // Include power ports for the Gate Level test:
 `ifdef GL_TEST
